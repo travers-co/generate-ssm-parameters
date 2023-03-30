ccdf01abc4d568088b98155c65ae613d6c6923c4
travers <travers@curiousorbit.com>
2023-03-30 11:46:31 -0400

    fix: create a sample csv

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="generate_ssm_parameters",
-    version="0.1.15",
+    version="0.1.16",
     description="A script for creating AWS SSM Parameters in bulk from a CSV file.",
     long_description=long_description,
     long_description_content_type="text/markdown",
